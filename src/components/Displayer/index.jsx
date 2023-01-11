ce1800d7dc4c0b3709473d0b392fd5acc8a2d567
yuanshikai524 <gcobc04992@gmail.com>
2023-01-12 02:42:04 +0800

    微調樣式

--- src/components/Displayer/index.jsx
+++ src/components/Displayer/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Displayer = ({ data }) => {
   return (
-    <ul style={{listStyleType: 'none', paddingLeft: '278px'}}>
+    <ul style={{listStyleType: 'none', padding: '32px 20px 32px 278px', backgroundColor: 'rgb(242, 242, 242, 0.7)'}}>
       {
         Object.entries(data).map(arr => <li key={arr[0]}><span style={{fontWeight: 'bold'}}>{arr[0]}</span>：&nbsp;<span>{arr[1]}</span></li>)
       }
